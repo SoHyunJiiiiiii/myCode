feeb6003f038f488a25b91b78dae932e7528eb4c
SoHyunJiiiiiii <thgus4545@daum.net>
2017-09-09 16:16:17 +0900

    Update 11403_경로찾기.cpp

--- algorithm/11403_경로찾기.cpp
+++ algorithm/11403_경로찾기.cpp
@@ -14,59 +14,26 @@ int n;//정점의 갯수
 int matrix[101][101];
 int visited[101][101];
 int v[101];
-vector<int> st;
-void check() {
+
+void DFS(int start,int finish) {
+
+    //printf("dfs : %d->%d\n",start,finish);
+    if(v[finish]==1)    return;
+    v[finish] = 1;
+    visited[start][finish]=1;
     
-    if(st.begin()==st.end()) {
-        for(int i=0; i<st.size()-1; i++) {
-            for(int j=0; j<st.size()-1; j++) {
-                visited[st[i]][st[j]]=1;
-            }
-        }
-    } else {
-        for(int i=0; i<st.size(); i++) {
-            for(int j=i+1; j<st.size(); j++) {
-                visited[st[i]][st[j]]=1;
-            }
+    for(int i=1; i<=n; i++) {
+        if(matrix[finish][i]==1) {
+            DFS(start, i);
         }
     }
-
-    st.pop_back();
-
     
 }
-void DFS(int start) {
-
-    
-    if(v[start]) {
-        st.push_back(start);
-        check();
-        st.pop_back();
-        return;
+void checkInit() {
+    for(int i=1; i<=n; i++) {
+        v[i] = 0;
     }
-
-    st.push_back(start);
-    v[start]=1;
-
-
-    int cnt=0;
-    for(int j=1; j<=n; j++) {
-        if(matrix[start][j]) {
-            cnt++;
-            //printf("push %d \n",j);
-            DFS(j);
-        }
-    }
-    if(cnt==0){
-        check();
-        return;
-    }
-    
-    
-    
-    
 }
-
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -75,15 +42,16 @@ int main(int argc, const char * argv[]) {
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
             scanf("%d",&matrix[i][j]);
+            visited[i][j]=0;
         }
     }
     
     for(int i=1; i<=n; i++) {
-        if(!v[i]) {
-            DFS(i);
-            st.clear();
-            memset(v, 0, sizeof(v));
-            v[i]=1;
+        for(int j=1; j<=n; j++) {
+            if(matrix[i][j]==1) {
+                checkInit();
+                DFS(i,j);
+            }
         }
     }
     
